1f0f03f4ed800ff9fb3707118dd139f3a153d6d5
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-15 10:55:36 +0000

    added sections and content placeholders to the LogOrSigninContainer

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import './App.css';
 
 function App() {
+  
+  
   return (
   <div className='App-wrapper'>
 
@@ -14,15 +16,29 @@ function App() {
 
     <div className='LogOrSigninContainer'>
       <div className='registerBox'>
+        <p className='promptText'>username</p>
+        <input></input>
+        <p className='promptText'>email</p>
+        <input></input>
+        <p className='promptText'>password</p>
+        <input></input>
+        <button type='submit'>Register User</button>
 
       </div>
       <div className='loginBox'>
+        <p className='promptText'>username</p>
+        <input></input>
+        <p className='promptText'>password</p>
+        <input></input>
+        <button type='submit'>Login</button>
 
       </div>
       <div className='logoutBox'>
+        <button>logout</button>
 
       </div>
       <div className='messageBox'>
+        <p>placeholder text: X has logged in/out</p>
 
       </div>
     </div>
