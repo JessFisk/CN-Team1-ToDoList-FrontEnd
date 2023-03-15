39d20042517364cee1715d139b633a7c963ecece
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-15 15:42:43 +0000

    made LogOrSignInContainer ready for components. Import paths not connected yet. All commented out

--- src/components/logOrSignContainer/LogOrSignContainer.js
+++ src/components/logOrSignContainer/LogOrSignContainer.js
@@ -1,31 +1,19 @@
+// import React from "react";
+// import "./LogOrSigninContainer.css";
+// import Register from "./" ;
+// import Login from "./" ;
+// import Logout from "./";
+// import Message from "./";
 
+// const LogOrSigninContainer = () => {
+//   return (
+//     <div className="registerBox">
+//       <Register user = {user} setUser ={setUser} />
+//       <Login user = {user} setUser = {setUser} />
+//       <Logout />
+//       <Message message = {message} setMessage = {setMessage} />
 
-        <div className='LogOrSigninContainer'>
-        <div className='registerBox'>
-        <p className='promptText'>username</p>
-        <input></input>
-        <p className='promptText'>email</p>
-        <input></input>
-        <p className='promptText'>password</p>
-        <input></input>
-        <button type='submit'>Register User</button>
-
-      </div>
-      <div className='loginBox'>
-        <p className='promptText'>username</p>
-        <input></input>
-        <p className='promptText'>password</p>
-        <input></input>
-        <button type='submit'>Login</button>
-
-      </div>
-      <div className='logoutBox'>
-        <button>logout</button>
-
-      </div>
-      <div className='messageBox'>
-        <h3>Message area</h3>
-        <p>placeholder text: X has logged in/out</p>
-
-      </div>
-    </div>
+//     </div>
+//   )
+// }
+// export default LogOrSigninContainer;
