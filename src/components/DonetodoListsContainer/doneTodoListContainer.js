48a725a74e0187f79ade3583d8355ff25fb80452
Jessica Fisk <fisk.jess@gmail.com>
2023-03-16 23:40:34 +0000

    updated general layout of main page to create styled app

---
+++ src/components/DonetodoListsContainer/doneTodoListContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./doneTodoListContainer.css"
+
+const DoneTodoContainer = () => {
+  return (
+    <div id="donetodoContainer">
+      <h3 id="DoneToDoHeader">Done To-Do List </h3>
+    </div>
+  )
+}
+
+export default DoneTodoContainer;
--- src/components/ToDoContainer/ToDoContainer.js
+++ src/components/ToDoContainer/ToDoContainer.js
@@ -1,3 +1,33 @@
+
+import React from "react";
+import "./ToDoContainer.css"
+
+const TodoContainer = () => {
+  return (
+    <div id="todoContainer">
+
+      <h3 id='ActiveToDoHeader'>Active To-Do List</h3>
+
+      <div className='TextArea'>
+        <label id='InputHeader'>New task: </label>
+        <input></input>
+        <button type='submit'>Add new task</button>
+      </div>
+    </div>
+  )
+}
+
+export default TodoContainer;
+
+
+
+
+
+
+
+
+
+
 {/* <div className='ToDoContainer'>
 <div className='ActiveToDo'>
   <h3 id='ActiveToDoHeader'>Active To-Do List</h3> 
